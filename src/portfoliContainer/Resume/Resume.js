e6bbaf357a2e15551e24cc02013ce0951cbef2e6
hemasudha <hemasudha.a@way2news.com>
2023-05-19 14:13:31 +0530

    second commit

--- src/portfoliContainer/Resume/Resume.js
+++ src/portfoliContainer/Resume/Resume.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Resume.css'
-export default function Resume() {
+import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading'
+import ScrollService from '../../utilities/ScrollService'
+import Animations from '../../utilities/Animations'
+export default function Resume(props) {
+    const [selectedBulletIndex, setSelectedBulletIndex] = useState(0);
+    const [carousalOffSetStyle, setCarousalOffSetStyle] = useState({})
+
+    let fadeInScreenHandler = (screen) => {
+        if (screen.fadeScreen !== props.id)
+            return
+        Animations.animations.fadeInScreen(props.id)
+    }
+    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+    console.log(fadeInSubscription);
     return (
-        <div>
+        <div className='resume-container screen-container ' id={props.id || ""}>
+            <div className='resume-content'>
+                <ScreenHeading title={'Resume'} subHeading={'My Formal Bio Details'} />
+
+            </div>
 
         </div>
     )
